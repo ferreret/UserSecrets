eec5802917ba976472c4320df7520a79bd51ea7e
Nicolás Barceló <ferreret@gmail.com>
2022-09-02 20:46:47 +0200

    Finished sample

---
+++ CommandConfig/Program.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.Configuration;
+
+IConfigurationBuilder builder = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true) // El orden de estas dos últimas intrucciones 
+    .AddUserSecrets(typeof(Program).Assembly, optional: true);             // define el orden donde irá a buscar las configuraciones
+
+IConfigurationRoot config = builder.Build();
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("I think that the password is: " + config["Password"]);
